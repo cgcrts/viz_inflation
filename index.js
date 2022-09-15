3fa95c1939023fa6780cc151122f31df5fbe66cb
cgcrts <cyrille.gay-crosier@rts.ch>
2022-09-15 18:47:59 +0200

    added data file + updates

--- index.js
+++ index.js
@@ -1,41 +1,51 @@
-const dataDetails = {
-    pain: {
-        img: 'bread.png',
-        product: 'Pain paysan',
-        shop: 'Migros',
-        brand: 'Boulangerie Migros',
-        quantity: '500g',
-        link: 'https://www.migros.ch',
-        priceBefore: '3',
-        priceNow: '3.30',
+let dataInflation;
+
+function setup () {
+    // Charger les données (Attention: opération asynchrone !)
+    loadData();
+}
+
+function loadData() {
+    // Attention, il s'agit d'une opération asynchrone !
+    // Une fois les données chargées, la promise sera résolue (.then) et
+    // le callback `onDataLoaded` sera appelé en passant les données en paramètre
+    Promise.all([
+        d3.dsv(';','data/inflation_data.csv'),
+    ]).then(function(files){
+        onDataLoaded(files)
+    })
+}
+
+function onDataLoaded(data) {
+    dataInflation = data[0]
+    console.log(dataInflation)
+    getItemData('aubergine')
+}
+
+function getItemData(item) {
+    for (const i in dataInflation) {
+        const itemObject = dataInflation[i]
+        if (itemObject.id === item) {
+            console.log(itemObject)
+            return itemObject
+        } else {
+            console.log('no item found')
+        }
     }
 }
-
-const labelDetails = {
-    product: 'Produit',
-    shop: 'Magasin',
-    brand: 'Marque',
-    quantity: 'Quantité',
-    link: 'Lien',
-    priceBefore: 'Prix initial',
-    priceNow: 'Prix actuel',
-    priceEvolution: 'Evolution du prix',
-    percentageEvolution: 'Evolution en %',
-}
-
-const priceLabels = [
-    'priceBefore', 'priceNow', 'priceEvolution', 'percentageEvolution',
-]
 
 function clickedItem(item) {
     document.getElementById('overlay').style.display = 'block'
     document.getElementById('detailsContainer').style.display = 'block'
     console.log('clicked', item)
+    getItemData(item)
 
-    const detailsTableLoc = document.getElementById('detailsList')
-    let detailsHTML = '<table>'
-    detailsHTML += '</table>'
-    //detailsTableLoc.innerHTML = detailsHTML
+    const itemIconLoc = document.getElementById('itemIcon')
+    itemIconLoc.innerHTML = `<img src="images/${item}.png" alt="">`
+
+    const detailsTableLoc = document.getElementById('detailsTable')
+    let detailsTableHTML = '<table>'
+    detailsTableHTML += '</table>'
 
     const priceBoxLoc = document.getElementById('priceBox')
     let priceHTML = ''
@@ -47,37 +57,50 @@ function clickedItem(item) {
 
 }
 
-function closeButton() {
+function closeDetailsWindow() {
     document.getElementById('overlay').style.display = 'none'
     document.getElementById('detailsContainer').style.display = 'none'
 }
 
-const labelsChart = [
-    'January',
-    'February',
-    'March',
-    'April',
-    'May',
-    'June',
-    ];
+const testData = {
+    '17.05': '1.7',
+    '01.06': '1.7',
+    '15.06': '2.2',
+    '01.07': '2.1',
+    '15.07': null,
+    '31.07': '2.1',
+    '16.08': '2.1',
+    '15.09': '2.4',
+}
 
 const dataChart = {
-    labels: labelsChart,
     datasets: [{
-        label: 'My First dataset',
-        backgroundColor: 'rgb(255, 99, 132)',
-        borderColor: 'rgb(255, 99, 132)',
-        data: [0, 10, 5, 2, 20, 30, 45],
+        label: 'Prix',
+        backgroundColor: '#dc2733',
+        borderColor: '#dc2733',
+        data: testData,
+        spanGaps: true,
 }]
 };
 
 const config = {
     type: 'line',
     data: dataChart,
-    options: {}
+    options: {
+        scales: {
+            yAxis: {min: 0, max: 5}
+        },
+        plugins: {
+            legend: {
+                display:false
+            }
+        }
+    }
 };
 
 const myChart = new Chart(
     document.getElementById('priceChart'),
     config
 );
+
+setup()
