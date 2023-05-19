e16cec6088456ffedb41b600c0be5d671d5675ad
cgcrts <108659711+cgcrts@users.noreply.github.com>
2023-05-19 17:09:05 +0200

    updated prices 2023-05-18

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ let dataInflation;
 let dataTranslation;
 let selectedItems = [];
 const language = 'fre'
-const dataFileName = 'data/inflation_data_updated_05_01.csv'
+const dataFileName = 'data/inflation_data_updated_05_18.csv'
 const translationFileName = 'data/translation.csv'
-const latestDate = '01.05.23'
+const latestDate = '18.05.23'
 const datesList = [
     '17.05.2022',
     '01.06.2022',
@@ -30,6 +30,7 @@ const datesList = [
     '01.04.2023',
     '14.04.2023',
     '01.05.2023',
+    '18.05.2023',
 ]
 let datesFormattedList = []
 for (const date of datesList) {
