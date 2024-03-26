a366fab3d7a98380f1ff2de0681a6290bbed8132
cgcrts <108659711+cgcrts@users.noreply.github.com>
2024-03-26 10:47:00 +0100

    updated prices 2024-03-22

--- index.js
+++ index.js
@@ -76,7 +76,7 @@ function onDataLoaded(data) {
     showGrid(dataInflation)
     generateReceiptDetails()
     updateLanguageLabels()
-    //startUpScreen()
+    startUpScreen()
 
     setTimeout(RTSInfoMisc.resize(), 200);
     setTimeout(resizeWindow(), 200)
